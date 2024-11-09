3bd987e9882bb5d0588a93ea62ea69ea1f575082
4poloo <m.olave.b7@gmail.com>
2024-11-09 12:19:14 -0300

    Cambios en posiciones de carpetas, se agrega logos e imagenes, se trabaja switch para modo noche, se comienza el maquetado de la pagina principal.

---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import lightPng from "../../assets/LightBtn.png"
+import DarkPng from "../../assets/DarkBtn.png"
+
+const DarkMode = () => {
+    const [theme, setTheme] = React.useState(localStorage.getItem("theme") ? localStorage.getItem("theme"): "light")
+
+    const element = document.documentElement // html element
+    //console.log(elemnt)
+    React.useEffect(()=> {
+        if(theme == "dark"){
+            element.classList.add("dark");
+            localStorage.setItem("theme","dark");
+        } else {
+            element.classList.remove("dark");
+            localStorage.setItem("theme","light");
+        }
+    }, [theme]);
+
+    return (
+        <div className='relative'>
+            <img src={lightPng} alt='' onClick={() => setTheme(theme == "light" ? "dark" : "light")} className={`w-16 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme == "dark"? "opacity-0" : "opacity-100"}`} />
+            <img src={DarkPng} alt='' onClick={() => setTheme(theme == "light" ? "dark" : "light")} className='w-16 cursor-pointer drop-shadow- transition-all duration-300' />
+        </div>
+    )
+}
+
+export default DarkMode;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         primary: "#1abc9c",
         secondary: "#58d68d",
-      }
+      },
+      container:{
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
     },
   },
   plugins: [],
